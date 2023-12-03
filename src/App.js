d231e513bcf969fb57b7dccdcd71e5286c9afcf4
AVUKU-PRAGATHESWARI <pragatheswaria@gmail.com>
2023-12-03 13:46:51 +0530

    Added navbar

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import './App.css';
 import Footer from './components/Footer/Footer';
 import Info from './components/Info/Info';
+import Navbar from './components/Navbar/Navbar';
 
 function App() {
 
   return (
-    <Footer/>
+    <Navbar/>
   );
   
 }
